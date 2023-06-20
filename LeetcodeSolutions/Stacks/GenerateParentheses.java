a17e69ea7bd95d43b91ba535f9bc5baa7fb2f23d
Ding <bding@devnet.local>
2023-06-20 11:42:53 -0700

    Added GenerateParentheses Solution

---
+++ LeetcodeSolutions/Stacks/GenerateParentheses.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.Stack;
+import java.util.ArrayList;
+
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        System.out.println(generateParentheses(3).toString());
+    }
+
+    public static List<String> generateParentheses(int n) {
+        List<String> result = new ArrayList<>();
+        generateParentheses(result, "", 0, 0, n);
+        return result;
+    }
+
+    private static void generateParentheses(List<String> result, String s, int open, int close, int n) {
+        if (open == n && close == n) {
+            result.add(s);
+            return;
+        }
+
+        if (open < n) {
+            generateParentheses(result, s + "(" , open + 1, close, n);
+        }
+
+        if (close < open) {
+            generateParentheses(result, s + ")", open, close + 1, n);
+        }
+    }
+}
