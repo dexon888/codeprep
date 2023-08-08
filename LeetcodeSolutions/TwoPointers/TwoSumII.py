a73c75b431683268c77fdf8a891d336ebce14e20
Brandon Ding <dexon888@gmail.com>
2023-08-08 02:08:24 -0700

    Added TwoSumII Solution

---
+++ LeetcodeSolutions/TwoPointers/TwoSumII.py
@@ -0,0 +1,19 @@
+def twoSum(numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+            else:
+                left += 1
+        return [left + 1, right + 1]
+
+numbers = [2,7,11,15]
+target = 9
+print(twoSum(numbers, target))
